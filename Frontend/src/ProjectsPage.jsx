fc7a6b3331cc8d4bb539dc5a973d65be0f1c3980
Hugo Eklund <eklund.hugo@gmail.com>
2025-07-10 13:48:33 +0200

    fixed theme import

--- Frontend/src/ProjectsPage.jsx
+++ Frontend/src/ProjectsPage.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactMarkdown from 'react-markdown';
-import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
-import { atomOneDark } from 'react-syntax-highlighter/dist/esm/styles/prism';
+import { Dark as SyntaxHighlighter } from 'react-syntax-highlighter';
+import { atomOneDark } from 'react-syntax-highlighter/dist/esm/styles/hljs'; 
 import { projects } from './ProjectsData';
 
 const ProjectsPage = ({ projectId, onBack }) => {
